9e44453f8d3291258012cec09665cdaefc28db46
Vinith Kumar <vinithkumar0720@gmail.com>
2025-06-04 15:10:52 +0530

    none: comments the code

--- Prisma/index.ts
+++ Prisma/index.ts
@@ -3,10 +3,10 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({ log: ["query", "error", "warn"] });
 
 async function main() {
-  const users = await prisma.tbl_gym_lists.findMany({
-    select: { name: true, email: true, address: true },
-  });
-  console.log(users);
+  // const users = await prisma.tbl_gym_lists.findMany({
+  //   select: { name: true, email: true, address: true },
+  // });
+  // console.log(users);
 }
 
 main()
