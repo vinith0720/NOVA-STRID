167a199f9c7faf952510a3c17d161aa6eaac9b5d
Vinith Kumar <vinithkumar0720@gmail.com>
2025-06-26 15:47:39 +0530

    prisma : client

--- PRISMA_PROJECT/src/client.ts
+++ PRISMA_PROJECT/src/client.ts
@@ -1,16 +1,8 @@
 // src/prisma/client.ts
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['query', 'error'], // optional
-  });
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+const prisma = new PrismaClient({
+  log: ['query', 'warn', 'error', 'info'],
+});
 
 export default prisma;
